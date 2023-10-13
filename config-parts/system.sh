d3f5eb3f10f12cf20d44f39554bc5020bd816277
kireque <5346530+kireque@users.noreply.github.com>
2023-10-13 09:06:18 +0200

    add nameserver

--- config-parts/system.sh
+++ config-parts/system.sh
@@ -10,7 +10,7 @@ set system host-name 'gateway'
 # set system login user vyos authentication public-keys personal key 'AAAAC3NzaC1lZDI1NTE5AAAAIMyYn4k4V+myBBl79Nt3t7EZugvz9A+d3ZbKyaP1w7J5'
 # set system login user vyos authentication public-keys personal type 'ssh-ed25519'
 
-# set system name-server '1.1.1.1'
+set system name-server '1.1.1.1'
 
 # set system sysctl parameter kernel.pty.max value '24000'
 
