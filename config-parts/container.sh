43d25a4b6878147510ab01fbd7af26422ca3abf3
kireque <5346530+kireque@users.noreply.github.com>
2023-10-13 11:46:01 +0200

    update config

--- config-parts/container.sh
+++ config-parts/container.sh
@@ -1,7 +1,7 @@
-# #!/bin/vbash
+#!/bin/vbash
 
-# # Container networks
-# set container network containers prefix '10.5.0.0/24'
+# Container networks
+set container network containers prefix '10.5.0.0/24'
 
 # # cloudflare-ddns
 # set container name cloudflare-ddns allow-host-networks
@@ -16,33 +16,33 @@
 # set container name cloudflare-ddns restart 'on-failure'
 # set container name cloudflare-ddns shared-memory '0'
 
-# # bind
-# set container name bind cap-add 'net-bind-service'
-# set container name bind image 'docker.io/internetsystemsconsortium/bind9:9.19'
-# set container name bind command '/usr/sbin/named -4 -f -c /etc/bind/named.conf -u bind'
-# set container name bind memory '0'
-# set container name bind network containers address '10.5.0.3'
-# set container name bind restart 'on-failure'
-# set container name bind shared-memory '0'
-# set container name bind volume config destination '/etc/bind'
-# set container name bind volume config source '/config/containers/bind/config'
-# set container name bind volume config mode 'ro'
-# set container name bind volume cache source '/tmp/bind/cache'
-# set container name bind volume cache destination '/var/cache/bind'
-# set container name bind volume cache mode 'rw'
+# bind
+set container name bind cap-add 'net-bind-service'
+set container name bind image 'docker.io/internetsystemsconsortium/bind9:9.19'
+set container name bind command '/usr/sbin/named -4 -f -c /etc/bind/named.conf -u bind'
+set container name bind memory '0'
+set container name bind network containers address '10.5.0.3'
+set container name bind restart 'on-failure'
+set container name bind shared-memory '0'
+set container name bind volume config destination '/etc/bind'
+set container name bind volume config source '/config/containers/bind/config'
+set container name bind volume config mode 'ro'
+set container name bind volume cache source '/tmp/bind/cache'
+set container name bind volume cache destination '/var/cache/bind'
+set container name bind volume cache mode 'rw'
 
-# # dnsdist
-# set container name dnsdist cap-add 'net-bind-service'
-# set container name dnsdist environment TZ value 'Europe/Amsterdam'
-# set container name dnsdist image 'docker.io/powerdns/dnsdist-18:1.8.1'
-# set container name dnsdist arguments '--log-timestamps'
-# set container name dnsdist memory '0'
-# set container name dnsdist network containers address '10.5.0.4'
-# set container name dnsdist restart 'on-failure'
-# set container name dnsdist shared-memory '0'
-# set container name dnsdist volume config source '/config/containers/dnsdist/config/dnsdist.conf'
-# set container name dnsdist volume config destination '/etc/dnsdist/dnsdist.conf'
-# set container name dnsdist volume config mode 'ro'
+# dnsdist
+set container name dnsdist cap-add 'net-bind-service'
+set container name dnsdist environment TZ value 'Europe/Amsterdam'
+set container name dnsdist image 'docker.io/powerdns/dnsdist-18:1.8.1'
+set container name dnsdist arguments '--log-timestamps'
+set container name dnsdist memory '0'
+set container name dnsdist network containers address '10.5.0.4'
+set container name dnsdist restart 'on-failure'
+set container name dnsdist shared-memory '0'
+set container name dnsdist volume config source '/config/containers/dnsdist/config/dnsdist.conf'
+set container name dnsdist volume config destination '/etc/dnsdist/dnsdist.conf'
+set container name dnsdist volume config mode 'ro'
 
 # # wildcard certificate
 # set container name lego-auto image 'ghcr.io/bjw-s/lego-auto:v0.1.0'
@@ -96,69 +96,69 @@
 # set container name pihole volume certificate-pem destination '/etc/lighttpd/certs/pihole.pem'
 # set container name pihole volume certificate-pem mode 'ro'
 
-# # haproxy-k8s-api
-# set container name haproxy-k8s-api image 'docker.io/library/haproxy:2.8.3'
-# set container name haproxy-k8s-api memory '0'
-# set container name haproxy-k8s-api network containers address '10.5.0.2'
-# set container name haproxy-k8s-api restart 'on-failure'
-# set container name haproxy-k8s-api shared-memory '0'
-# set container name haproxy-k8s-api volume config source '/config/containers/haproxy/config/haproxy.cfg'
-# set container name haproxy-k8s-api volume config destination '/usr/local/etc/haproxy/haproxy.cfg'
-# set container name haproxy-k8s-api volume config mode 'ro'
+# haproxy-k8s-api
+set container name haproxy-k8s-api image 'docker.io/library/haproxy:2.8.3'
+set container name haproxy-k8s-api memory '0'
+set container name haproxy-k8s-api network containers address '10.5.0.2'
+set container name haproxy-k8s-api restart 'on-failure'
+set container name haproxy-k8s-api shared-memory '0'
+set container name haproxy-k8s-api volume config source '/config/containers/haproxy/config/haproxy.cfg'
+set container name haproxy-k8s-api volume config destination '/usr/local/etc/haproxy/haproxy.cfg'
+set container name haproxy-k8s-api volume config mode 'ro'
 
-# # node-exporter
-# set container name node-exporter environment procfs value '/host/proc'
-# set container name node-exporter environment rootfs value '/host/rootfs'
-# set container name node-exporter environment sysfs value '/host/sys'
-# set container name node-exporter image 'quay.io/prometheus/node-exporter:v1.6.1'
-# set container name node-exporter memory '0'
-# set container name node-exporter allow-host-networks
-# set container name node-exporter restart 'on-failure'
-# set container name node-exporter shared-memory '0'
-# set container name node-exporter volume procfs source '/proc'
-# set container name node-exporter volume procfs destination '/host/proc'
-# set container name node-exporter volume procfs mode 'ro'
-# set container name node-exporter volume rootfs source '/'
-# set container name node-exporter volume rootfs destination '/host/rootfs'
-# set container name node-exporter volume rootfs mode 'ro'
-# set container name node-exporter volume sysfs source '/sys'
-# set container name node-exporter volume sysfs destination '/host/sys'
-# set container name node-exporter volume sysfs mode 'ro'
+# node-exporter
+set container name node-exporter environment procfs value '/host/proc'
+set container name node-exporter environment rootfs value '/host/rootfs'
+set container name node-exporter environment sysfs value '/host/sys'
+set container name node-exporter image 'quay.io/prometheus/node-exporter:v1.6.1'
+set container name node-exporter memory '0'
+set container name node-exporter allow-host-networks
+set container name node-exporter restart 'on-failure'
+set container name node-exporter shared-memory '0'
+set container name node-exporter volume procfs source '/proc'
+set container name node-exporter volume procfs destination '/host/proc'
+set container name node-exporter volume procfs mode 'ro'
+set container name node-exporter volume rootfs source '/'
+set container name node-exporter volume rootfs destination '/host/rootfs'
+set container name node-exporter volume rootfs mode 'ro'
+set container name node-exporter volume sysfs source '/sys'
+set container name node-exporter volume sysfs destination '/host/sys'
+set container name node-exporter volume sysfs mode 'ro'
 
-# # speedtest-exporter
-# set container name speedtest-exporter image 'ghcr.io/miguelndecarvalho/speedtest-exporter:v3.5.4'
-# set container name speedtest-exporter memory '0'
-# set container name speedtest-exporter allow-host-networks
-# set container name speedtest-exporter restart 'on-failure'
-# set container name speedtest-exporter shared-memory '0'
+# speedtest-exporter
+set container name speedtest-exporter image 'ghcr.io/miguelndecarvalho/speedtest-exporter:v3.5.4'
+set container name speedtest-exporter memory '0'
+set container name speedtest-exporter allow-host-networks
+set container name speedtest-exporter restart 'on-failure'
+set container name speedtest-exporter shared-memory '0'
 
-# # udp-broadcast-relay-mdns
-# set container name udp-broadcast-relay-mdns allow-host-networks
-# set container name udp-broadcast-relay-mdns cap-add 'net-raw'
-# set container name udp-broadcast-relay-mdns environment CFG_DEV value 'eth1.20;eth1.40'
-# set container name udp-broadcast-relay-mdns environment CFG_ID value '2'
-# set container name udp-broadcast-relay-mdns environment CFG_MULTICAST value '224.0.0.251'
-# set container name udp-broadcast-relay-mdns environment CFG_PORT value '5353'
-# set container name udp-broadcast-relay-mdns environment SEPARATOR value ';'
-# set container name udp-broadcast-relay-mdns image 'ghcr.io/onedr0p/udp-broadcast-relay-redux:1.0.27'
-# set container name udp-broadcast-relay-mdns memory '0'
-# set container name udp-broadcast-relay-mdns restart 'on-failure'
-# set container name udp-broadcast-relay-mdns shared-memory '0'
+# udp-broadcast-relay-mdns
+set container name udp-broadcast-relay-mdns allow-host-networks
+set container name udp-broadcast-relay-mdns cap-add 'net-raw'
+set container name udp-broadcast-relay-mdns environment CFG_DEV value 'eth1.20;eth1.40'
+set container name udp-broadcast-relay-mdns environment CFG_ID value '2'
+set container name udp-broadcast-relay-mdns environment CFG_MULTICAST value '224.0.0.251'
+set container name udp-broadcast-relay-mdns environment CFG_PORT value '5353'
+set container name udp-broadcast-relay-mdns environment SEPARATOR value ';'
+set container name udp-broadcast-relay-mdns image 'ghcr.io/onedr0p/udp-broadcast-relay-redux:1.0.27'
+set container name udp-broadcast-relay-mdns memory '0'
+set container name udp-broadcast-relay-mdns restart 'on-failure'
+set container name udp-broadcast-relay-mdns shared-memory '0'
 
-# # unifi
-# set container name unifi environment RUNAS_UID0 value 'false'
-# set container name unifi environment TZ value 'Europe/Amsterdam'
-# set container name unifi environment UNIFI_GID value '999'
-# set container name unifi environment UNIFI_STDOUT value 'true'
-# set container name unifi environment UNIFI_UID value '999'
-# set container name unifi image 'ghcr.io/jacobalberty/unifi-docker:v7.5.174'
-# set container name unifi memory '0'
-# set container name unifi network containers address '10.5.0.10'
-# set container name unifi restart 'on-failure'
-# set container name unifi shared-memory '0'
-# set container name unifi volume data source '/config/containers/unifi'
-# set container name unifi volume data destination '/unifi'
-# set container name unifi volume data mode 'rw'
+# unifi
+set container name unifi environment RUNAS_UID0 value 'false'
+set container name unifi environment TZ value 'Europe/Amsterdam'
+set container name unifi environment UNIFI_GID value '999'
+set container name unifi environment UNIFI_STDOUT value 'true'
+set container name unifi environment UNIFI_UID value '999'
+set container name unifi image 'ghcr.io/jacobalberty/unifi-docker:v7.5.174'
+set container name unifi memory '0'
+set container name unifi network containers address '10.5.0.10'
+set container name unifi restart 'on-failure'
+set container name unifi shared-memory '0'
+set container name unifi volume data source '/config/containers/unifi'
+set container name unifi volume data destination '/unifi'
+set container name unifi volume data mode 'rw'
 
 # # onepassword-connect
 # set container name onepassword-connect image 'docker.io/1password/connect-api:1.7.2'
