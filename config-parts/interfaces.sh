82b5624f803e887350ff8887bafca1fa4b7230d5
kireque <5346530+kireque@users.noreply.github.com>
2023-10-13 08:48:08 +0200

    comment out everything

--- config-parts/interfaces.sh
+++ config-parts/interfaces.sh
@@ -1,35 +1,35 @@
-#!/bin/vbash
+# #!/bin/vbash
 
-set interfaces ethernet eth0 description 'WAN - Fiber'
-set interfaces ethernet eth0 hw-id 'e4:3a:6e:5f:33:9c'
-set interfaces ethernet eth0 mtu '1508'
-set interfaces ethernet eth0 vif 6 description 'Internet'
-set interfaces ethernet eth0 vif 6 mtu '1508'
+# set interfaces ethernet eth0 description 'WAN - Fiber'
+# set interfaces ethernet eth0 hw-id 'e4:3a:6e:5f:33:9c'
+# set interfaces ethernet eth0 mtu '1508'
+# set interfaces ethernet eth0 vif 6 description 'Internet'
+# set interfaces ethernet eth0 vif 6 mtu '1508'
 
-set interfaces ethernet eth1 address '10.1.0.1/24'
-set interfaces ethernet eth1 description 'LAN'
-set interfaces ethernet eth1 hw-id 'e4:3a:6e:5f:33:9d'
-set interfaces ethernet eth1 vif 10 address '10.1.1.1/24'
-set interfaces ethernet eth1 vif 10 description 'SERVERS'
-set interfaces ethernet eth1 vif 20 address '10.1.2.1/24'
-set interfaces ethernet eth1 vif 20 description 'TRUSTED'
-set interfaces ethernet eth1 vif 30 address '192.168.2.1/24'
-set interfaces ethernet eth1 vif 30 description 'GUEST'
-set interfaces ethernet eth1 vif 40 address '10.1.3.1/24'
-set interfaces ethernet eth1 vif 40 description 'IOT'
-set interfaces ethernet eth1 vif 50 address '10.1.4.1/24'
-set interfaces ethernet eth1 vif 50 description 'VIDEO'
+# set interfaces ethernet eth1 address '10.1.0.1/24'
+# set interfaces ethernet eth1 description 'LAN'
+# set interfaces ethernet eth1 hw-id 'e4:3a:6e:5f:33:9d'
+# set interfaces ethernet eth1 vif 10 address '10.1.1.1/24'
+# set interfaces ethernet eth1 vif 10 description 'SERVERS'
+# set interfaces ethernet eth1 vif 20 address '10.1.2.1/24'
+# set interfaces ethernet eth1 vif 20 description 'TRUSTED'
+# set interfaces ethernet eth1 vif 30 address '192.168.2.1/24'
+# set interfaces ethernet eth1 vif 30 description 'GUEST'
+# set interfaces ethernet eth1 vif 40 address '10.1.3.1/24'
+# set interfaces ethernet eth1 vif 40 description 'IOT'
+# set interfaces ethernet eth1 vif 50 address '10.1.4.1/24'
+# set interfaces ethernet eth1 vif 50 description 'VIDEO'
 
-set interfaces pppoe pppoe0 description 'WAN - Fiber'
-set interfaces pppoe pppoe0 source-interface 'eth0.6'
-set interfaces pppoe pppoe0 mtu '1500'
-set interfaces pppoe pppoe0 authentication user "${SECRET_ISP_AUTH_USER}"
-set interfaces pppoe pppoe0 authentication password "${SECRET_ISP_AUTH_PASSWORD}"
+# set interfaces pppoe pppoe0 description 'WAN - Fiber'
+# set interfaces pppoe pppoe0 source-interface 'eth0.6'
+# set interfaces pppoe pppoe0 mtu '1500'
+# set interfaces pppoe pppoe0 authentication user "${SECRET_ISP_AUTH_USER}"
+# set interfaces pppoe pppoe0 authentication password "${SECRET_ISP_AUTH_PASSWORD}"
 
-set interfaces wireguard wg01 address '10.0.11.1/24'
-set interfaces wireguard wg01 description 'WIREGUARD'
-set interfaces wireguard wg01 peer oneplus-8t allowed-ips '10.0.11.4/32'
-set interfaces wireguard wg01 peer oneplus-8t persistent-keepalive '15'
-set interfaces wireguard wg01 peer oneplus-8t public-key '8WyIUVFoOCP14rAK/s7aUareIH2O/5kIgu/lvWPbZnA='
-set interfaces wireguard wg01 port '51820'
-set interfaces wireguard wg01 private-key "${SECRET_WIREGUARD_PRIVATE_KEY}"
+# set interfaces wireguard wg01 address '10.0.11.1/24'
+# set interfaces wireguard wg01 description 'WIREGUARD'
+# set interfaces wireguard wg01 peer oneplus-8t allowed-ips '10.0.11.4/32'
+# set interfaces wireguard wg01 peer oneplus-8t persistent-keepalive '15'
+# set interfaces wireguard wg01 peer oneplus-8t public-key '8WyIUVFoOCP14rAK/s7aUareIH2O/5kIgu/lvWPbZnA='
+# set interfaces wireguard wg01 port '51820'
+# set interfaces wireguard wg01 private-key "${SECRET_WIREGUARD_PRIVATE_KEY}"
--- config-parts/service.sh
+++ config-parts/service.sh
@@ -1,30 +1,30 @@
-#!/bin/vbash
+# #!/bin/vbash
 
-# NTP server
-delete service ntp allow-client
-set service ntp allow-client address '127.0.0.0/8'
-set service ntp allow-client address '10.0.0.0/8'
-set service ntp allow-client address '172.16.0.0/12'
-set service ntp allow-client address '192.168.0.0/16'
-delete service ntp server
-set service ntp server nl.pool.ntp.org
+# # NTP server
+# delete service ntp allow-client
+# set service ntp allow-client address '127.0.0.0/8'
+# set service ntp allow-client address '10.0.0.0/8'
+# set service ntp allow-client address '172.16.0.0/12'
+# set service ntp allow-client address '192.168.0.0/16'
+# delete service ntp server
+# set service ntp server nl.pool.ntp.org
 
-# SSH server
-set service ssh disable-password-authentication
-set service ssh port '22'
+# # SSH server
+# set service ssh disable-password-authentication
+# set service ssh port '22'
 
-# UDP Broadcast-Relay
-set service broadcast-relay id 1 description 'Bambu 1'
-set service broadcast-relay id 1 interface 'eth1.20'
-set service broadcast-relay id 1 interface 'eth1.40'
-set service broadcast-relay id 1 port '2021'
+# # UDP Broadcast-Relay
+# set service broadcast-relay id 1 description 'Bambu 1'
+# set service broadcast-relay id 1 interface 'eth1.20'
+# set service broadcast-relay id 1 interface 'eth1.40'
+# set service broadcast-relay id 1 port '2021'
 
-set service broadcast-relay id 2 description 'Bambu 2'
-set service broadcast-relay id 2 interface 'eth1.20'
-set service broadcast-relay id 2 interface 'eth1.40'
-set service broadcast-relay id 2 port '1990'
+# set service broadcast-relay id 2 description 'Bambu 2'
+# set service broadcast-relay id 2 interface 'eth1.20'
+# set service broadcast-relay id 2 interface 'eth1.40'
+# set service broadcast-relay id 2 port '1990'
 
-set service broadcast-relay id 3 description 'Sonos'
-set service broadcast-relay id 3 interface 'eth1.20'
-set service broadcast-relay id 3 interface 'eth1.40'
-set service broadcast-relay id 3 port '1900'
+# set service broadcast-relay id 3 description 'Sonos'
+# set service broadcast-relay id 3 interface 'eth1.20'
+# set service broadcast-relay id 3 interface 'eth1.40'
+# set service broadcast-relay id 3 port '1900'
