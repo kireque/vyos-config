a02f0b3351bef5366f72ff4ba386c3ed2d53895a
kireque <5346530+kireque@users.noreply.github.com>
2023-10-13 09:32:46 +0200

    add commit

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -42,3 +42,5 @@ set nat source rule 100 description 'LAN -> WAN'
 set nat source rule 100 outbound-interface 'eth1'
 set nat source rule 100 destination address '0.0.0.0/0'
 set nat source rule 100 translation address 'masquerade'
+
+commit;save
