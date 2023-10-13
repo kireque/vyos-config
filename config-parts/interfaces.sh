e59f2c14abdbc2ab089058555b1070ad32e9d012
kireque <5346530+kireque@users.noreply.github.com>
2023-10-13 10:05:45 +0200

    test config

--- config-parts/interfaces.sh
+++ config-parts/interfaces.sh
@@ -1,30 +1,30 @@
 # #!/bin/vbash
 
-# set interfaces ethernet eth0 description 'WAN - Fiber'
-# set interfaces ethernet eth0 hw-id 'e4:3a:6e:5f:33:9c'
-# set interfaces ethernet eth0 mtu '1508'
-# set interfaces ethernet eth0 vif 6 description 'Internet'
-# set interfaces ethernet eth0 vif 6 mtu '1508'
+set interfaces ethernet eth1 description 'WAN - Fiber'
+set interfaces ethernet eth1 hw-id 'e4:3a:6e:5f:33:9c'
+set interfaces ethernet eth1 mtu '1508'
+set interfaces ethernet eth1 vif 6 description 'Internet'
+set interfaces ethernet eth1 vif 6 mtu '1508'
 
-# set interfaces ethernet eth1 address '10.1.0.1/24'
-# set interfaces ethernet eth1 description 'LAN'
-# set interfaces ethernet eth1 hw-id 'e4:3a:6e:5f:33:9d'
-# set interfaces ethernet eth1 vif 10 address '10.1.1.1/24'
-# set interfaces ethernet eth1 vif 10 description 'SERVERS'
-# set interfaces ethernet eth1 vif 20 address '10.1.2.1/24'
-# set interfaces ethernet eth1 vif 20 description 'TRUSTED'
-# set interfaces ethernet eth1 vif 30 address '192.168.2.1/24'
-# set interfaces ethernet eth1 vif 30 description 'GUEST'
-# set interfaces ethernet eth1 vif 40 address '10.1.3.1/24'
-# set interfaces ethernet eth1 vif 40 description 'IOT'
-# set interfaces ethernet eth1 vif 50 address '10.1.4.1/24'
-# set interfaces ethernet eth1 vif 50 description 'VIDEO'
+set interfaces ethernet eth0 address '10.1.0.1/24'
+set interfaces ethernet eth0 description 'LAN'
+set interfaces ethernet eth0 hw-id 'e4:3a:6e:5f:33:9d'
+set interfaces ethernet eth0 vif 10 address '10.1.1.1/24'
+set interfaces ethernet eth0 vif 10 description 'SERVERS'
+set interfaces ethernet eth0 vif 20 address '10.1.2.1/24'
+set interfaces ethernet eth0 vif 20 description 'TRUSTED'
+set interfaces ethernet eth0 vif 30 address '192.168.2.1/24'
+set interfaces ethernet eth0 vif 30 description 'GUEST'
+set interfaces ethernet eth0 vif 40 address '10.1.3.1/24'
+set interfaces ethernet eth0 vif 40 description 'IOT'
+set interfaces ethernet eth0 vif 50 address '10.1.4.1/24'
+set interfaces ethernet eth0 vif 50 description 'VIDEO'
 
-# set interfaces pppoe pppoe0 description 'WAN - Fiber'
-# set interfaces pppoe pppoe0 source-interface 'eth0.6'
-# set interfaces pppoe pppoe0 mtu '1500'
-# set interfaces pppoe pppoe0 authentication user "${SECRET_ISP_AUTH_USER}"
-# set interfaces pppoe pppoe0 authentication password "${SECRET_ISP_AUTH_PASSWORD}"
+set interfaces pppoe pppoe0 description 'WAN - Fiber'
+set interfaces pppoe pppoe0 source-interface 'eth0.6'
+set interfaces pppoe pppoe0 mtu '1500'
+set interfaces pppoe pppoe0 authentication user "${SECRET_ISP_AUTH_USER}"
+set interfaces pppoe pppoe0 authentication password "${SECRET_ISP_AUTH_PASSWORD}"
 
 # set interfaces wireguard wg01 address '10.0.11.1/24'
 # set interfaces wireguard wg01 description 'WIREGUARD'
--- config-parts/service.sh
+++ config-parts/service.sh
@@ -1,17 +1,17 @@
 # #!/bin/vbash
 
-# # NTP server
-# delete service ntp allow-client
-# set service ntp allow-client address '127.0.0.0/8'
-# set service ntp allow-client address '10.0.0.0/8'
-# set service ntp allow-client address '172.16.0.0/12'
-# set service ntp allow-client address '192.168.0.0/16'
-# delete service ntp server
-# set service ntp server nl.pool.ntp.org
+# NTP server
+delete service ntp allow-client
+set service ntp allow-client address '127.0.0.0/8'
+set service ntp allow-client address '10.0.0.0/8'
+set service ntp allow-client address '172.16.0.0/12'
+set service ntp allow-client address '192.168.0.0/16'
+delete service ntp server
+set service ntp server nl.pool.ntp.org
 
 # # SSH server
-# set service ssh disable-password-authentication
-# set service ssh port '22'
+set service ssh disable-password-authentication
+set service ssh port '22'
 
 # # UDP Broadcast-Relay
 # set service broadcast-relay id 1 description 'Bambu 1'
