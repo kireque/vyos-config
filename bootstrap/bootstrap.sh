df8bbf70df0fde565d3db7642739cb95e5602a34
kireque <5346530+kireque@users.noreply.github.com>
2023-10-13 09:08:28 +0200

    update bootstrap

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -3,7 +3,7 @@
 load /opt/vyatta/etc/config.boot.default
 
 set interfaces ethernet eth0 description 'LAN'
-set interfaces ethernet eth0 address '10.1.0.1/24'
+set interfaces ethernet eth0 address '192.168.1.10/23'
 
 set interfaces ethernet eth1 description 'WAN - Cable'
 set interfaces ethernet eth1 address 'dhcp'
@@ -26,13 +26,13 @@ set system sysctl parameter kernel.pty.max value '24000'
 
 set system time-zone 'Europe/Amsterdam'
 
-set service dhcp-server shared-network-name LAN authoritative
-set service dhcp-server shared-network-name LAN ping-check
-set service dhcp-server shared-network-name LAN subnet 10.1.0.0/24 default-router '10.1.0.1'
-set service dhcp-server shared-network-name LAN subnet 10.1.0.0/24 lease '86400'
-set service dhcp-server shared-network-name LAN subnet 10.1.0.0/24 name-server '1.1.1.1'
-set service dhcp-server shared-network-name LAN subnet 10.1.0.0/24 range 0 start '10.1.0.200'
-set service dhcp-server shared-network-name LAN subnet 10.1.0.0/24 range 0 stop '10.1.0.254'
+# set service dhcp-server shared-network-name LAN authoritative
+# set service dhcp-server shared-network-name LAN ping-check
+# set service dhcp-server shared-network-name LAN subnet 10.1.0.0/24 default-router '10.1.0.1'
+# set service dhcp-server shared-network-name LAN subnet 10.1.0.0/24 lease '86400'
+# set service dhcp-server shared-network-name LAN subnet 10.1.0.0/24 name-server '1.1.1.1'
+# set service dhcp-server shared-network-name LAN subnet 10.1.0.0/24 range 0 start '10.1.0.200'
+# set service dhcp-server shared-network-name LAN subnet 10.1.0.0/24 range 0 stop '10.1.0.254'
 
 # ALL -> WAN masquerade
 set nat source rule 100 description 'LAN -> WAN'
