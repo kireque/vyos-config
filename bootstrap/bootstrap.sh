4f58a069613701c1db8d71090d709b4fb6b2065a
kireque <5346530+kireque@users.noreply.github.com>
2023-11-09 12:40:39 +0100

    update nat

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -48,8 +48,7 @@ set service dhcp-server shared-network-name LAN subnet 10.1.0.0/24 range 0 stop 
 
 # ALL -> WAN masquerade
 set nat source rule 100 description 'LAN -> WAN'
-set nat source rule 100 outbound-interface 'eth0'
-set nat source rule 100 destination address '0.0.0.0/0'
+set nat source rule 100 outbound-interface name'pppoe0'
 set nat source rule 100 translation address 'masquerade'
 
 commit;save
